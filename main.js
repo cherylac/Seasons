353c490a3d229a19acaf695a3f202a84bb597372
Cheryl Campbell <cherylcmpbll@yahoo.com>
2018-08-27 04:32:02 +0000

    Added initial html, js, css

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-main js file
+//JS for 3020 Final Project - "Seasons App"///
+
+    var d = new Date();
+document.getElementById("date").innerHTML = d;
+
+
+if (new Date().getHours() < 18) {
+    document.getElementById("greeting").innerHTML = "Good day!";
+} 
+    else {
+        document.getElementById("greeting").innerHTML = "Good evening!";
+    }
+    
+//TODO: add 2 event listeners.
+
+//TODO: use an Array and an Object.
+
+//TODO: Make use of either Functions or Classes to organize code.
+
+//TODO: Perform at least TWO string modifications/replacements on the page.
+
+//TODO: Leverage proper DOM traversal and understanding of hierarchy to facilitate manipulating the page to enhance the user's experience.
+
